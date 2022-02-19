a21e6e1636870789531553fde362c05bff62da7d
Dmitry Budolak <maniacmasnion12@gmail.com>
2022-02-19 21:28:53 +0300

    фиксация

---
+++ фигня/1.py
@@ -0,0 +1,7 @@
+import numpy as np
+np.finfo(np.float16)
+# finfo(resolution=0.001, min=-6.55040e+04, max=6.55040e+04, dtype=float16)
+np.finfo(np.float32)
+# finfo(resolution=1e-06, min=-3.4028235e+38, max=3.4028235e+38, dtype=float32)
+np.finfo(np.float64)
+# finfo(resolution=1e-15, min=-1.7976931348623157e+308, max=1.7976931348623157e+308, dtype=float64)
